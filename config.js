fb2a0a0d7b4fe36561df3a19fc94be94978e9b40
ngovanhieu <82451911+ngovanhieu@users.noreply.github.com>
2021-07-01 15:41:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'do you love me ?',
+    mess: 'Anh biết mà là em cũng thích anh mà',
+    messDesc: ' vậy thôi mình ib nch nha . acc anh là Ngô Hiếu ',
     btnAccept: 'yes.. <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
