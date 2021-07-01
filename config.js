6cbb94a2bc72f14c92c703e3b7491f7959706c7c
ngovanhieu <82451911+ngovanhieu@users.noreply.github.com>
2021-07-01 15:45:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Em à!',
+    introTitle: 'Em à, là anh đây anh muốn nói với em vài điều thôi !',
     introDesc: `Người ta nói cuộc đời này có hai thứ ta không nên bỏ lỡ
     1 là chuyến xe cuối cùng về nhà 
     2 là người thật lòng yêu thương mình`,
     btnIntro: 'Bấm vào đây nè ... ',
     title: 'Anh Hy vọng em đừng bỏ lỡ anh😙',
-    desc: 'You are the apple of my eye ',
-    btnYes: 'Em Thích anh à ? <33',
-    btnNo: 'Không, Anh trai à :3',
+    desc: 'Do you love me?  ',
+    btnYes: ' yess <33',
+    btnNo: 'Không, Anh mơ à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà là em cũng thích anh mà',
-    messDesc: ' vậy thôi mình ib nch nha . acc anh là Ngô Hiếu ',
-    btnAccept: 'yes.. <3',
+    messDesc: ' vậy thôi mình ib nói chuyện nha . acc anh là Ngô Hiếu ',
+    btnAccept: 'dạ anh <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
